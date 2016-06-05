486830965d238440cb03df280389a16fb2494df6
Vinicius Freitas <vinimmbb@gmail.com>
2016-06-05 19:33:20 -0300

    Lets make it make

---
+++ src/ONode.cpp
@@ -0,0 +1,15 @@
+/* created by Vinicius Freitas<vinicius.mctf@grad.ufsc.br> 
+   and Marleson Graf<aszdrick@gmail.com> [2016] */
+
+#include "ONode.hpp"
+
+ONode::ONode() : public DSNode() { }
+
+void ONode::down_action() {
+
+
+}
+
+void ONode::up_action() {
+	
+}
