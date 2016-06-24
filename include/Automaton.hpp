16d0f4eb6f9a48d2444a8859c877671c2c74aaae
aszdrick <aszdrick@gmail.com>
2016-06-24 17:21:51 -0300

    forgot

---
+++ include/Automaton.hpp
@@ -0,0 +1,145 @@
+/* created by Vinicius Freitas<vinicius.mctf@grad.ufsc.br> 
+   and Marleson Graf<aszdrick@gmail.com> [2016] */
+
+/**
+ *
+ * Automaton class definition:
+ *  Esta classe define um autômato de estados finitos,
+ *	o reconhecedor para uma expressão, gramática ou
+ *	linguagem regular.
+ *  Estão aqui implementadas as operações sobre autômatos,
+ *	união, complemento, remoção de estados mortos e inalcansáveis,
+ *	além do algoritmo de minimização e a operação de igualdade.
+ **/
+
+#ifndef FS_MACHINE_HPP
+#define FS_MACHINE_HPP
+
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+class Automaton {
+ public:
+ 	/* Construtor vazio da máquina de estados */
+ 	Automaton(); 
+    
+ 	/* Construtor passando alfabeto como parâmetro */
+    Automaton(const std::set<char>&);
+    
+    /* Método para inserção de estado na máquina: label, inicial, final */
+    void insert(const std::string&, bool = false, bool = false);
+
+    /* Método para criar uma transição de from por label para to: from, label, to */
+    void make_transition(const std::string&, const char, const std::string&);
+
+ 	/* Remove os estados que não levam ao estado final */
+    void remove_dead_states();
+    
+    /* Remove os estados inalcansáveis a partir do estado inicial */
+    void remove_unreachable_states();
+
+    /* Troca os estados finais e não finais
+     * Faz o complemento do autômato
+     * NF -> F
+     * F -> NF
+     */
+    Automaton complement();
+
+    /* Cria um novo estado inicial que tem as mesmas 
+     * transições dos estados iniciais das duas máquinas
+     * e devolve uma nova máquina com esta característica
+     */
+    Automaton union_operation(const Automaton&);
+
+    /* Executa o algoritmo de minimização de autômato finito */
+    Automaton minimize();
+
+    /* Transforma o autômato numa tabela, para impressão em terminal/console */
+    operator std::string() const;
+
+    std::vector<std::string>& operator()(const std::string&, const char);
+
+    const std::vector<std::string>& operator()(const std::string&, const char) const;
+
+    std::vector<std::vector<std::string>> to_table();
+
+ private:
+    class State;
+
+ 	/* Um mapa de nome para estado, contendo todos os estados do autômato */
+    std::map<std::string, State> states;
+
+    /* O alfabeto da máquina de estados (sigma) */
+    std::set<char> alphabet;
+
+    /* O nome do estado inicial */
+    std::string initial_state;
+
+    /* Um conjunto dos nomes dos estados finais */ 
+    std::set<std::string> final_states;
+
+    std::string rejection_state;
+
+    bool is_dead(const State&) const;
+};
+
+class Automaton::State {
+ public:
+    /*
+     * Construtor de estado, padrão: 
+     * (label epslon, sem rejeição, não inicial, não final)
+     */
+    State(bool = false, bool = false);
+
+    /* Verifica se estado é inicial */
+    bool is_initial() const;
+
+    /* Verifica se estado é final */
+    bool is_final() const;
+
+    /* Verifica se estado aceita a entrada fornecida.
+     * i.e., se existe transição com dada entrada,
+     * ignorando as transições para o estado de rejeição.
+     */
+    bool accepts(const char) const;
+
+    void append_transition(const char, const std::string&);
+
+    std::map<char,std::vector<std::string>>::iterator begin();
+
+    std::map<char,std::vector<std::string>>::iterator end();
+
+    std::map<char,std::vector<std::string>>::const_iterator begin() const;
+
+    std::map<char,std::vector<std::string>>::const_iterator end() const;
+
+    /* Acesso a uma transição existente. */
+    std::vector<std::string>& operator[](const char);
+
+    /* Acesso const a uma transição existente. */
+    const std::vector<std::string>& operator[](const char) const;
+
+    operator std::string() const;
+
+ private:
+    /* Label do estado */
+    std::string label;
+
+    /* Mapa de transições */
+    std::map<char, std::vector<std::string>> transitions;
+
+    /* Identifica se estado é inicial.
+     * True se inicial, false caso contrário.
+     */
+    bool initial;
+    
+    /* Identifica se estado é final.
+     * True se final, false caso contrário.
+     */
+    bool final;
+};
+
+#endif /* FS_MACHINE_HPP */
---
+++ src/Automaton.cpp
@@ -0,0 +1,276 @@
+/* created by Vinicius Freitas<vinicius.mctf@grad.ufsc.br> 
+   and Marleson Graf<aszdrick@gmail.com> [2016] */
+
+#include "Automaton.hpp"
+#include <iostream>
+#include <algorithm>
+
+Automaton::Automaton() { }
+
+Automaton::Automaton(const std::set<char>& alphabet) : alphabet(alphabet) {
+
+}
+
+void Automaton::insert(const std::string& entry, bool initial, bool final) {
+    states[entry] = State(initial, final);
+    if (initial) initial_state = entry;
+    if (final) final_states.insert(entry);
+}
+
+void Automaton::make_transition(const std::string& from, char label,
+                                const std::string& to) {
+    if (!states.count(to)) states[to];
+    states[from].append_transition(label, to);
+}
+
+std::vector<std::string>& Automaton::operator()(const std::string& state,
+                                                const char entry) {
+    return states[state][entry];
+
+}
+
+const std::vector<std::string>& Automaton::operator()(const std::string& state,
+                                                      const char entry) const {
+    return states.at(state)[entry];
+}
+
+
+void Automaton::remove_dead_states() {
+    unsigned i = 0;
+    auto pair = states.begin();
+    while (i < states.size()) {
+        if (is_dead(pair->second)) {
+            states.erase(pair);
+            pair = states.begin();
+        } else {
+            i++;
+            pair++;
+        }
+    }
+    // states.erase(std::remove_if(states.begin(), states.end(),
+    //             [this](const std::pair<std::string,State>& pair)->bool{
+    //                 return this->is_dead(pair.second);
+    //             }), states.end());
+}
+
+bool Automaton::is_dead(const State& state) const {
+        if (state.is_final()) {
+        return false;
+    }
+    for (auto pair : state) {
+        for (auto target : pair.second) {
+            if (!is_dead(states.at(target))) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Automaton::remove_unreachable_states() {
+    std::map<std::string, State> reachable_states;
+    reachable_states[initial_state] = states[initial_state];
+    for (auto pair : states) {
+        if (!reachable_states.count(pair.first)) {
+            bool reachable = false;
+            for (auto rpair : reachable_states) {
+                for (auto transition : rpair.second) {
+                    for (auto t : transition.second) {
+                        if (t == pair.first) {
+                            reachable = true;
+                            break;
+                        }
+                    }
+                    if (reachable) break;
+                }
+                if (reachable) break;
+            }
+            if (reachable) {
+                reachable_states.insert(pair);
+            }
+        }
+    }
+    // for (auto pair : reachable_states) {
+    //     for (auto t : alphabet) {
+    //         for (auto s : pair.second) {
+    //             reachable_states[s] = states[s];
+    //         }
+    //     }
+    // }
+    states = reachable_states;
+}
+
+Automaton Automaton::complement() {
+    std::set<std::string> non_final_states;
+    for (auto pair : states) {
+        if (!pair.second.is_final()) {
+            non_final_states.insert(pair.first);
+        }
+    }
+
+    Automaton complemented = *this;
+    complemented.final_states = non_final_states;
+    return complemented;
+}
+
+Automaton Automaton::union_operation(const Automaton& fsm) {
+    // std::set<char> united_alphabet;
+    // for (auto c : alphabet) {
+    //     united_alphabet.insert(c);
+    // }
+    // for (auto c : fsm.alphabet) {
+    //     united_alphabet.insert(c);
+    // }
+    // Automaton united_machine (united_machine);
+    
+    // bool initial_is_final = false;
+    // if (final_states.count(initial_state) && fsm.final_states.count(fsm.initial_state)) {
+    //     initial_is_final = true;
+    // }
+    
+    // std::string initial_label = initial_state+"\'"+"\'";
+    // State initial (initial_label, nullptr, true, initial_is_final);
+    // for (auto st : states) {
+    //     united_machine[st.first] = st.second;
+    // }
+    // for (auto st : fsm.states) {
+    //     st.second.set_label(st.first+"\'");
+    //     united_machine[st.first+"\'"] = st.second;
+    // }
+    // united_machine[initial_label] = initial;
+    // united_machine.initial_state = initial_label;
+
+    // for (auto c : united_machine.alphabet) {
+    //     if (!united_machine[initial_state][c].empty()) {
+    //         for (auto t : united_machine[initial_state][c]) {
+    //             united_machine.make_transition(initial_label, c, t->get_label());
+    //         }
+    //     }
+    //     if (!united_machine[initial_state+"\'"][c].empty()) {
+    //         for (auto t : united_machine[initial_state+"\'"][c]) {
+    //             united_machine.make_transition(initial_label, c, t->get_label());
+    //         }
+    //     }
+    // }
+
+    // return united_machine;
+}
+
+Automaton Automaton::minimize() {
+    // Automaton minimezed_automata = *this;
+    // minimezed_automata.remove_unreachable_states();
+    // minimezed_automata.remove_dead_states();
+    // for (auto t : minimezed_automata.alphabet) {
+    //     for (auto st : minimezed_automata.states) {
+    //         if (!st.second.accepts(t)) st.second[t].push_back(&minimezed_automata.rejection_state);
+    //     }
+    // }
+    // std::map<std::string, std::vector<State>> state_set;
+    // state_set["\u03D5"].push_back(minimezed_automata.rejection_state);
+    // std::string state_count = "A";
+    // for (auto st : final_states) {
+    //     state_set[state_count].push_back(minimezed_automata[st]);
+    // }
+    // state_count.at(0)++;
+    /*while (true) {
+        for (auto st : state_set) {
+            if (st.second.size() > 1) {
+                for (unsigned i = 1; i < st.second.size()-1; i++) {
+                    for (auto a : minimezed_automata.alphabet) {
+                        std::string dest, cest;
+                        // This shit is confusing
+                        dest = ((*st.second.begin())[a].at(0).get_label());
+                        cest = ((*(st.second.begin()+i))[a].at(0).get_label());
+                        for (auto ts : state_set) {
+                        	for (auto k : ts.second) {
+                        		if (k.get_transition(a).get_label() == dest) dest = "\'"+ts.first;
+                        		if (k.get_transition(a).get_label() == cest) cest = "\'"+ts.first;
+                        	}
+                        }
+                        if (dest != cest) {
+                        	State diff_state = *st.second.begin();
+                        	st.second.erase(st.second.begin()+i);
+                        	state_set[state_count].push_back(diff_state);
+                        }
+                    }
+                }   
+            }
+        }
+        state_count.at(0)++;
+    }*/
+
+    //return minimezed_automata;
+}
+
+Automaton::operator std::string() const {
+    // std::string out = "      ";
+    // for (auto entry : alphabet) {
+    //     out += "| ";
+    //     out += entry;
+    //     out += " ";
+    // }
+    // out += "\n";
+    // for (auto s : states) {
+    //     out += " ";
+    //     if (s.second.is_final()) out += "*";
+    //     else out += " ";
+    //     if (s.second.is_initial()) out += "->";
+    //     else out += "  ";
+    //     out += s.second.get_label();
+    //     out += " ";
+    //     for (auto entry : alphabet) {
+    //         bool test = true;
+    //         out += "| ";
+    //         for (auto t : s.second[entry]) {
+    //             test = false;
+    //             out += t->get_label();
+    //         }
+    //         if (test) out += " ";
+    //         out += " ";
+    //     }
+    //     out += "\n";
+    // }
+    // return out;
+}
+
+std::vector<std::vector<std::string>> Automaton::to_table() {
+    std::vector<std::vector<std::string>> result;
+    
+    result.push_back({"\u03B4"});
+    
+    for (char entry : alphabet) {
+        result[0].push_back(std::string(1, entry));
+    }
+    
+    unsigned i = 1;
+    for (auto pair : states) {
+        std::string state_str = "";
+        
+        if (pair.second.is_final()) {
+            state_str += "*";
+        }
+        if (pair.second.is_initial()) {
+            state_str += "->";
+        }
+        
+        state_str += pair.first;
+        
+        result.push_back({state_str});
+        
+        for (auto entry : alphabet) {
+            std::string transition_str = "";
+        
+            for (auto s : pair.second[entry]) {
+                transition_str += s;
+            }
+        
+            if (transition_str.size() == 0) {
+                transition_str += "\u2014";
+            }
+        
+            result[i].push_back(transition_str);
+        }
+        i++;
+    }
+    return result;
+}
