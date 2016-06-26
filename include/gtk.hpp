da4804a8f19efcd43be7890629f401c577c20260
aszdrick <aszdrick@gmail.com>
2016-06-26 17:27:40 -0300

    useless things gone

--- include/gtk.hpp
+++ include/gtk.hpp
@@ -10,70 +10,8 @@
 #include <vector>
 
 namespace gtk {
-    struct box_pack {
-        box_pack(GtkWidget* w, bool a, bool e = false, int o = false)
-        : widget(w), alloc(a), expand(e), offset(o) { }
-        box_pack(GtkWidget* w, int o = 0)
-        : widget(w), alloc(false), expand(false), offset(o) { }
-        GtkWidget* widget;
-        bool alloc;
-        bool expand;
-        int offset;
-    };
-
-    using menu_item_pack = std::pair<const char*, void (*)()>;
 
     void init(int, char**);
-
-    void queue_draw(GtkWidget*);
-
-    GtkWidget* new_window(const char*, bool = true, int = 0);
-
-    GtkWidget* new_scrolled_window(GtkAdjustment* = NULL, GtkAdjustment* = NULL, 
-                                   const GtkShadowType& = GTK_SHADOW_NONE, int = -1,
-                                   int = -1, int = 0);
-
-    GtkWidget* new_dialog(const GtkWidget*, const char*, int = 0);
-
-    GtkWidget* new_paned(const GtkOrientation& = GTK_ORIENTATION_HORIZONTAL,
-                         GtkWidget* = NULL, bool = false, bool = false,
-                         GtkWidget* = NULL, bool = false, bool = false);
-
-    GtkWidget* new_paned(const GtkOrientation& = GTK_ORIENTATION_HORIZONTAL,
-                         GtkWidget* = NULL, GtkWidget* = NULL);
-
-    GtkWidget* new_frame(const char*, float = 0, float = 0.5, int = 0);
-
-    GtkWidget* new_entry(const std::string& = "", float = 0, int = 0, int = 50);
-
-    GtkWidget* new_button(const char*, GtkWidget* = NULL, void (*)() = NULL);
-
-    GtkWidget* new_box(const GtkOrientation& = GTK_ORIENTATION_HORIZONTAL,
-                       const GtkWidget* = NULL, int = 0, bool = false, int = 0);
-
-    GtkWidget* new_grid(const GtkWidget* = NULL, int = 1, int = 1, bool = false,
-                        bool = false, int = 0);
-
-    GtkWidget* new_list_box(GtkWidget* const = NULL);
-
-    GtkWidget* new_button_box(const GtkOrientation& = GTK_ORIENTATION_HORIZONTAL,
-                              bool = true, int = 0);
-
-    GtkWidget* new_expander(const char*, const bool = true);
-
-    GtkWidget* new_expander_with_frame(const char*, const bool = true,
-                                       const GtkShadowType& = GTK_SHADOW_ETCHED_IN);
-
-    void set_entry_max_length(const GtkWidget*, int, int = 50);
-
-    void box_push_back(const GtkWidget*, const std::vector<box_pack>&);
-
-    void box_push_front(const GtkWidget*, const std::vector<box_pack>&);
-
-    void box_push(const std::vector<box_pack>&,
-                   const std::function<void(GtkWidget*, bool, bool, int)>&);
-
-    void set_margins(GtkWidget*, int = 0, int = 0, int = 0, int = 0);
 
     void main();
 
